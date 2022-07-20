aae76d44d252b2845b4486545a2772bea9c67483
Your Name <your.email@example.com>
2022-07-20 11:48:52 -0600

    env adjust

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require("morgan")
 const mongoose = require("mongoose")
 const expressJwt = require("express-jwt")
 const path = require("path")
+const port = process.env.PORT || 9000
 
 app.use(express.json())
 app.use(morgan("dev"))
@@ -36,7 +37,7 @@ mongoose.connect(
 
 
 app.use("/auth", require("./routes/authRouter.js"))
-app.use("/api", expressJwt({secret: process.env.SECRET || "abracadabra sizzle oops lalala", algorithms: ['HS256']}))  //creates req.user -- ALSO:  algorithms: for express-jwt v6.0.0 & higher: adding an algorithm parameter is now required in addition to the secret.
+app.use("/api", expressJwt({secret: process.env.SECRET, algorithms: ['HS256']}))  //creates req.user -- ALSO:  algorithms: for express-jwt v6.0.0 & higher: adding an algorithm parameter is now required in addition to the secret.
 app.use("/api/issue", require("./routes/issueRouter.js"))
 app.use("/api/comment", require("./routes/commentRouter.js"))
 
@@ -51,6 +52,6 @@ app.use((err, req, res, next) => {
 
 app.get("*", (req, res)=> {res.sendFile(path.join(_dirname, "client", "build", "index.html"))})
 
-app.listen(process.env.PORT || 9000, () => {
+app.listen(port, () => {
     console.log("Server is running on local port 9000")
 })
