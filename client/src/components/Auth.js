9565eb06e4aca0b58a7ee311bea0e0bae2ca2620
N R <ramoskel343@gmail.com>
2022-10-31 19:05:29 -0600

    move up sign-in

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -47,6 +47,14 @@ export default function Auth() {
           <p onClick={toggleForm} className="member-or-not">
             Already a member?
           </p>
+
+          <div className="sign-in-instructions">
+            <div className="title-instructions">SIGN-IN</div>
+            <div className="instruct">Click the "Already a member?" button</div>
+            <div className="instruct">and login with the following:</div>
+            <div className="login-instruct">username: guest</div>
+            <div className="login-instruct">password: guest</div>
+          </div>
         </>
       ) : (
         <>
@@ -62,13 +70,6 @@ export default function Auth() {
           </p>
         </>
       )}
-      <div className="sign-in-instructions">
-        <div className="title-instructions">SIGN-IN</div>
-        <div className="instruct">Click the "Already a member?" button</div>
-        <div className="instruct">and login with the following:</div>
-        <div className="login-instruct">username: guest</div>
-        <div className="login-instruct">password: guest</div>
-      </div>
     </div>
   );
 }
