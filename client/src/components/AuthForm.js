c52e1f2ad8edb4986df473f70fa1f07a1339892c
N R <ramoskel343@gmail.com>
2022-10-31 19:25:26 -0600

    move again

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -13,22 +13,26 @@ export default function AuthForm(props){
   } = props
   
   return (
-    <form className="auth-form" onSubmit={handleSubmit}>
-      <h1 className="app-title">RTV App</h1>
-      <input
-        type="text"
-        value={username}
-        name="username"
-        onChange={handleChange}
-        placeholder="Username"
-      />
-      <input
-        type="text"
-        value={password}
-        name="password"
-        onChange={handleChange}
-        placeholder="Password"
-      />
+    <div>
+      <form className="auth-form" onSubmit={handleSubmit}>
+        <h1 className="app-title">RTV App</h1>
+        <input
+          type="text"
+          value={username}
+          name="username"
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <input
+          type="text"
+          value={password}
+          name="password"
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        <button className="sign-up-btn">{btnText}</button>
+        <p style={{ color: "red" }}>{errMsg}</p>
+      </form>
 
       <div className="sign-in-instructions">
         <div className="title-instructions">SIGN-IN</div>
@@ -37,8 +41,6 @@ export default function AuthForm(props){
         <div className="login-instruct">username: guest</div>
         <div className="login-instruct">password: guest</div>
       </div>
-      <button className="sign-up-btn">{btnText}</button>
-      <p style={{ color: "red" }}>{errMsg}</p>
-    </form>
+    </div>
   );
 }
