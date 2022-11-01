37ef455cc60ae49156e026d6db04210a870ca697
N R <ramoskel343@gmail.com>
2022-10-31 18:50:15 -0600

    try div at end

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,37 +1,36 @@
-import React, { useState, useContext } from 'react'
-import AuthForm from './AuthForm.js'
-import { UserContext } from "../context/UserProvider.js"
+import React, { useState, useContext } from "react";
+import AuthForm from "./AuthForm.js";
+import { UserContext } from "../context/UserProvider.js";
 
-const initInputs = { username: "", password: ""}
+const initInputs = { username: "", password: "" };
 
-export default function Auth(){
-  const [inputs, setInputs] = useState(initInputs)
-  const [toggle, setToggle] = useState(false)
+export default function Auth() {
+  const [inputs, setInputs] = useState(initInputs);
+  const [toggle, setToggle] = useState(false);
 
-  const {signup, login, errMsg, resetAuthErr } = useContext(UserContext)
+  const { signup, login, errMsg, resetAuthErr } = useContext(UserContext);
 
-
-  function handleChange(e){
-    const {name, value} = e.target
-    setInputs(prevInputs => ({
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({
       ...prevInputs,
-      [name]: value
-    }))
+      [name]: value,
+    }));
   }
 
-  function handleSignup(e){
-    e.preventDefault()
-    signup(inputs)
+  function handleSignup(e) {
+    e.preventDefault();
+    signup(inputs);
   }
 
-  function handleLogin(e){
-    e.preventDefault()
-    login(inputs)
+  function handleLogin(e) {
+    e.preventDefault();
+    login(inputs);
   }
 
-  function toggleForm(){
-    setToggle(prev => !prev)
-    resetAuthErr()
+  function toggleForm() {
+    setToggle((prev) => !prev);
+    resetAuthErr();
   }
 
   return (
@@ -64,12 +63,12 @@ export default function Auth(){
         </>
       )}
       <div className="sign-in-instructions">
-            <div className="title-instructions">SIGN-IN</div>
-            <div className="instruct">Click the "Already a member?" button</div>
-            <div className="instruct">and login with the following:</div>
-            <div className="login-instruct">username: guest</div>
-            <div className="login-instruct">password: guest</div>
-          </div>
+        <div className="title-instructions">SIGN-IN</div>
+        <div className="instruct">Click the "Already a member?" button</div>
+        <div className="instruct">and login with the following:</div>
+        <div className="login-instruct">username: guest</div>
+        <div className="login-instruct">password: guest</div>
+      </div>
     </div>
   );
 }
